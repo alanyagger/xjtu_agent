fdfc9061b7408f730c1e0b4c14bb17af72520044
LYMQY <848356491@qq.com>
2025-07-04 16:55:35 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import HeaderView from "@/components/HeaderView.vue";
+</script>
+
+<template>
+  <section class="page-layout">
+    <!-- 头部 -->
+    <header class="page-layout-header">
+      <div class="page-layout-row">
+        <!-- 引入公共的头部组件 -->
+        <HeaderView />
+      </div>
+    </header>
+    <!-- 主体 -->
+    <main class="page-layout-content">
+      <section class="starter-content">
+        <div class="page-layout-row">
+          <RouterView />
+        </div>
+      </section>
+    </main>
+  </section>
+</template>
+
+<style scoped>
+.page-layout {
+  display: flex;
+  flex-direction: column;
+  background: #eee;
+
+  .page-layout-header {
+    display: flex;
+    justify-content: center;
+    min-width: 760px;
+    height: 66px;
+    background: #fff;
+    border-bottom: 1px solid #eee;
+    box-shadow: 0 2px 8px 0 rgba(2, 24, 42, 0.1);
+  }
+
+  .page-layout-row {
+    width: 1440px;
+    display: flex;
+    background: #fff;
+    flex-direction: column;
+  }
+
+  .page-layout-content {
+    flex: 1;
+  }
+
+  .starter-content {
+    height: calc(100vh - 66px);
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+// 讯飞星火大模型的配置
+export const sparkConfig = {
+  APPID: "6483403d",
+  APISecret: "ZmRlNGUxMjMwZTcxZTVhZTYwOWNjZWE3",
+  APIKey: "9a4eefb7835fd4065f624bee6c308048",
+  VERSION: "v1", // v1.1  v2.1  v3.1 x1
+};
