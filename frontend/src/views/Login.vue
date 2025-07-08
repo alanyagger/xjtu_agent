a829a6051994446d1604773587d42c281a5b3e66
zzy <513266084@qq.com>
2025-07-08 16:20:17 +0800

    Update Login.vue
    修改登录后页面

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -308,6 +308,7 @@ const handleLogin = () => {
         });
         // 存Token到localStorage
         localStorage.setItem('token', res.access_token);
+        localStorage.setItem('username', loginForm.username);
         ElMessage.success('登录成功');
         router.push('/aiChat'); // 跳转到聊天页
       } catch (error) {
